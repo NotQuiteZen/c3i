2fcfbf53e1d131cb1cd08157e0c85dac054ad75c
Milan <milanvanas@gmail.com>
2018-04-29 16:04:58 +0200

    Moved c3i install scripts to a seperate Console file and added some small improvements

---
+++ src/Console/C3I.php
@@ -0,0 +1,275 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     3.0.0
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace App\Console;
+
+if ( ! defined('STDIN')) {
+    define('STDIN', fopen('php://stdin', 'r'));
+}
+
+use Cake\Core\Configure;
+use Cake\Datasource\ConnectionManager;
+use Cake\Utility\Security;
+use Composer\IO\IOInterface;
+use Composer\Script\Event;
+use Exception;
+
+class C3I {
+
+
+    /**
+     * Does some routine installation tasks so people don't have to.
+     *
+     * @param \Composer\Script\Event $event The composer event object.
+     *
+     * @throws \Exception Exception raised by validator.
+     * @return mixed
+     */
+    public static function setupDatabase(Event $event) {
+
+        $io = $event->getIO();
+
+        # No interactive run? stop
+        if ( ! $io->isInteractive()) {
+            return static::_returnSleep();
+        }
+
+        # Get the root directory
+        $rootDir = dirname(dirname(__DIR__));
+
+        #
+        $appConfigFile = $rootDir . '/config/app.php';
+
+        $configContents = file_get_contents($appConfigFile);
+
+        if (strpos($configContents, '__DATABASE_USER__') === false) {
+            $io->write('<comment>Skipping database setup</comment>: No database tags found in ' . $appConfigFile);
+
+            return static::_returnSleep();
+        }
+
+        # Ask if we want to setup a database
+        $startDatabaseSequence = $io->askAndValidate(
+            '<info>Do you want to setup a database in your config? (Default to N)</info> [<comment>y,N</comment>]? ',
+            static::_yesNoValidator(),
+            10,
+            'N'
+        );
+
+        if ($startDatabaseSequence === 'n') {
+            return static::_returnSleep();
+        }
+
+        # Get the database user
+        $databaseUser = $io->askAndValidate(
+            '<info>Database username</info>: ',
+            static::_notEmptyValidator(),
+            10
+        );
+
+        # Get the database password
+        $databasePassword = $io->askAndValidate(
+            '<info>Database password <comment>(will not be hidden!)</comment></info>: ',
+            static::_notEmptyValidator(),
+            10
+        );
+
+        # Get the database name
+        $databaseName = $io->askAndValidate(
+            '<info>Database name</info>: ',
+            static::_notEmptyValidator(),
+            10
+        );
+
+        #
+        # Ask if we're happy with this
+        $happyWithDatabaseInput = $io->askAndValidate(
+            '<info>Are you happy with your input for the database configuration? (Default to Y)</info> [<comment>Y,n</comment>]?',
+            static::_yesNoValidator(),
+            10,
+            'Y'
+        );
+
+        # Not happy, lets stop
+        if (in_array($happyWithDatabaseInput, ['N', 'n'])) {
+            $io->write('Cancelling database setup.');
+
+            return static::_returnSleep();
+        }
+
+        # Replace the user, password and name in the app.php
+        static::_replaceTagInFile($io, '__DATABASE_USER__', $databaseUser, $appConfigFile);
+        static::_replaceTagInFile($io, '__DATABASE_PASSWORD__', $databasePassword, $appConfigFile);
+        static::_replaceTagInFile($io, '__DATABASE_NAME__', $databaseName, $appConfigFile);
+
+        return static::_returnSleep();
+    }
+
+
+    /**
+     * @param $event Event
+     *
+     * @throws Exception
+     */
+
+    public static function databaseSessions($event) {
+
+        $io = $event->getIO();
+
+        $rootDir = dirname(dirname(__DIR__));
+
+        # Bootstrap Cake
+        /** @noinspection PhpIncludeInspection */
+        require $rootDir . '/vendor/autoload.php';
+        /** @noinspection PhpIncludeInspection */
+        require $rootDir . '/config/bootstrap.php';
+
+        if (Configure::read('Session.defaults') !== 'php') {
+            $io->write('<comment>Skipping database sessions</comment>: Seems like you\'re not using default php sessions');
+
+            return static::_returnSleep();
+        }
+
+        # Ask if we want to use database sessions
+        $doWeWantDatabaseSessions = $io->askAndValidate(
+            "<info>Do you want to setup database sessions? (Default to N)</info> [<comment>y,N</comment>]?",
+            static::_yesNoValidator(),
+            10,
+            'N'
+        );
+
+        # If we dont want database sessions, just return, no hard feelings
+        if ($doWeWantDatabaseSessions === 'n') {
+            return static::_returnSleep();
+        }
+
+        # Get the sessions query from the file cake provided us with
+        $sessionTableQuery = file_get_contents($rootDir . '/config/schema/sessions.sql');
+
+        try {
+
+            # Get the db connection
+            $conn = ConnectionManager::get('default');
+
+            # Execute the query
+            $conn->execute($sessionTableQuery);
+
+        } catch (Exception $e) {
+            $io->write('<error>Something went wrong creationg the sessions table, perhaps the table already exists?</error>');
+
+            return static::_returnSleep();
+        }
+
+        # Set session config to 'database' instead of php
+        static::_replaceTagInFile($io, "'defaults' => 'php',", "'defaults' => 'database',", $rootDir . '/config/app.php');
+
+        # If all went well, and get here, we have no errors
+        $io->write('<info>Succesfully setup database sessions</info>');
+
+        return static::_returnSleep();
+    }
+
+    /**
+     * @param $io IOInterface
+     */
+    public static function logo(Event $event) {
+
+        $io = $event->getIO();
+
+        $io->write('');
+        $io->write('<info> ██████╗██████╗ ██╗    ███████╗███████╗████████╗██╗   ██╗██████╗</info>');
+        $io->write('<info>██╔════╝╚════██╗██║    ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗</info>');
+        $io->write('<info>██║      █████╔╝██║    ███████╗█████╗     ██║   ██║   ██║██████╔╝</info>');
+        $io->write('<info>██║      ╚═══██╗██║    ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝</info>');
+        $io->write('<info>╚██████╗██████╔╝██║    ███████║███████╗   ██║   ╚██████╔╝██║</info>');
+        $io->write('<info> ╚═════╝╚═════╝ ╚═╝    ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝</info>');
+        $io->write('');
+
+        return static::_returnSleep();
+    }
+
+    /**
+     * @param Event $event
+     */
+    public static function notifyYarnTime(Event $event) {
+
+        $io = $event->getIO();
+
+        $io->write('');
+        $io->write('Installing Node dependencies for our front-end and running a first-time build, this may take a while...');
+        $io->write('');
+    }
+
+    /**
+     * @param $io IOInterface
+     * @param $tag
+     * @param $replaceWith
+     * @param $file
+     */
+    private static function _replaceTagInFile($io, $tag, $replaceWith, $file) {
+        $content = file_get_contents($file);
+        $content = str_replace($tag, $replaceWith, $content, $count);
+
+        if ($count == 0) {
+            $io->write('No ' . $tag . ' placeholder to replace.');
+
+            return static::_returnSleep();
+        }
+
+        $result = file_put_contents($file, $content);
+        if ($result) {
+            $io->write('Updated ' . $tag . ' value in ' . $file);
+
+            return static::_returnSleep();
+        }
+        $io->write('Unable to update ' . $tag . ' value.');
+    }
+
+
+    /**
+     * @return \Closure
+     */
+    private static function _yesNoValidator() {
+        return function($arg) {
+            $arg = strtolower($arg);
+            if (in_array($arg, ['y', 'n'])) {
+                return $arg;
+            }
+            throw new Exception('This is not a valid answer. Please choose Y, y, N or n.');
+        };
+    }
+
+
+    /**
+     * Returns a not empty check function
+     * @return \Closure
+     */
+    private static function _notEmptyValidator() {
+        return function($arg) {
+            if ( ! empty($arg)) {
+                return $arg;
+            }
+            throw new Exception('Invalid input received, please insert a non-empty string');
+        };
+    }
+
+
+    /**
+     * @return int
+     */
+    private static function _returnSleep() {
+        return sleep(1);
+    }
+}
--- src/Console/Installer.php
+++ src/Console/Installer.php
@@ -19,11 +19,7 @@ if ( ! defined('STDIN')) {
     define('STDIN', fopen('php://stdin', 'r'));
 }
 
-use Cake\Core\Configure;
-use Cake\Core\Configure\Engine\PhpConfig;
-use Cake\Datasource\ConnectionManager;
 use Cake\Utility\Security;
-use Composer\IO\IOInterface;
 use Composer\Script\Event;
 use Exception;
 
@@ -60,29 +56,20 @@ class Installer {
 
         $rootDir = dirname(dirname(__DIR__));
 
-        # Output c3iLogo
-        static::c3iLogo($io);
-
-        # Create Config
         static::createAppConfig($rootDir, $io);
-
-        # Create writable dirs
         static::createWritableDirectories($rootDir, $io);
 
-        # Ask if the permissions should be changed
+        // ask if the permissions should be changed
         if ($io->isInteractive()) {
-
-
-            $yesNoValidator = function($arg) {
+            $validator = function($arg) {
                 if (in_array($arg, ['Y', 'y', 'N', 'n'])) {
                     return $arg;
                 }
-                throw new Exception('This is not a valid answer. Please choose Y, y, N or n.');
+                throw new Exception('This is not a valid answer. Please choose Y or n.');
             };
-
             $setFolderPermissions = $io->askAndValidate(
                 '<info>Set Folder Permissions ? (Default to Y)</info> [<comment>Y,n</comment>]? ',
-                $yesNoValidator,
+                $validator,
                 10,
                 'Y'
             );
@@ -90,23 +77,10 @@ class Installer {
             if (in_array($setFolderPermissions, ['Y', 'y'])) {
                 static::setFolderPermissions($rootDir, $io);
             }
-
-            $startDatabaseSequence = $io->askAndValidate(
-                '<info>Do you want to setup a database in your config? (Default to N)</info> [<comment>N,y</comment>]? ',
-                $yesNoValidator,
-                10,
-                'Y'
-            );
-
-            if (in_array($startDatabaseSequence, ['Y', 'y'])) {
-                static::startDatabaseSequence($rootDir, $io);
-            }
         } else {
-            # Set folder permissing
             static::setFolderPermissions($rootDir, $io);
         }
 
-        # Set Security Salt
         static::setSecuritySalt($rootDir, $io);
 
         if (class_exists('\Cake\Codeception\Console\Installer')) {
@@ -161,14 +135,14 @@ class Installer {
      */
     public static function setFolderPermissions($dir, $io) {
         // Change the permissions on a path and output the results.
-        $changePerms = function($path, $perms, $io) {
-            // Get permission bits from stat(2) result.
+        $changePerms = function($path) use ($io) {
             $currentPerms = fileperms($path)&0777;
-            if (($currentPerms&$perms) == $perms) {
+            $worldWritable = $currentPerms|0007;
+            if ($worldWritable == $currentPerms) {
                 return;
             }
 
-            $res = chmod($path, $currentPerms|$perms);
+            $res = chmod($path, $worldWritable);
             if ($res) {
                 $io->write('Permissions set on ' . $path);
             } else {
@@ -176,7 +150,7 @@ class Installer {
             }
         };
 
-        $walker = function($dir, $perms, $io) use (&$walker, $changePerms) {
+        $walker = function($dir) use (&$walker, $changePerms) {
             $files = array_diff(scandir($dir), ['.', '..']);
             foreach ($files as $file) {
                 $path = $dir . '/' . $file;
@@ -185,15 +159,14 @@ class Installer {
                     continue;
                 }
 
-                $changePerms($path, $perms, $io);
-                $walker($path, $perms, $io);
+                $changePerms($path);
+                $walker($path);
             }
         };
 
-        $worldWritable = bindec('0000000111');
-        $walker($dir . '/tmp', $worldWritable, $io);
-        $changePerms($dir . '/tmp', $worldWritable, $io);
-        $changePerms($dir . '/logs', $worldWritable, $io);
+        $walker($dir . '/tmp');
+        $changePerms($dir . '/tmp');
+        $changePerms($dir . '/logs');
     }
 
     /**
@@ -268,226 +241,5 @@ class Installer {
             return;
         }
         $io->write('Unable to update __APP_NAME__ value.');
-    }
-
-
-    /**
-     * @param $io IOInterface
-     */
-    public static function c3iLogo($io) {
-        $io->write('');
-        $io->write('<info> ██████╗██████╗ ██╗    ███████╗███████╗████████╗██╗   ██╗██████╗</info>');
-        $io->write('<info>██╔════╝╚════██╗██║    ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗</info>');
-        $io->write('<info>██║      █████╔╝██║    ███████╗█████╗     ██║   ██║   ██║██████╔╝</info>');
-        $io->write('<info>██║      ╚═══██╗██║    ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝</info>');
-        $io->write('<info>╚██████╗██████╔╝██║    ███████║███████╗   ██║   ╚██████╔╝██║</info>');
-        $io->write('<info> ╚═════╝╚═════╝ ╚═╝    ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝</info>');
-        $io->write('');
-    }
-
-
-    /**
-     * @param $dir
-     * @param $io IOInterface
-     *
-     * @throws Exception
-     */
-    public static function startDatabaseSequence($dir, $io) {
-
-        #
-        $appConfigFile = $dir . '/config/app.php';
-
-        $configContents = file_get_contents($appConfigFile);
-
-        if (strpos($configContents, '__DATABASE_USER__') === false) {
-            $io->write('No database tags found in ' . $appConfigFile . ', skipping database setup');
-
-        } else {
-
-
-            # Not empty validation function
-            $notEmptyValidator = function($arg) {
-                if ( ! empty($arg)) {
-                    return $arg;
-                }
-                throw new Exception('Invalid input received, please insert a non-empty string');
-            };
-
-            # Get the database user
-            $databaseUser = $io->askAndValidate(
-                '<info>Database username</info>: ',
-                $notEmptyValidator,
-                10
-            );
-
-            # Get the database password
-            $databasePassword = $io->askAndValidate(
-                '<info>Database password <comment>(will not be hidden!)</comment></info>: ',
-                $notEmptyValidator,
-                10
-            );
-
-            # Get the database name
-            $databaseName = $io->askAndValidate(
-                '<info>Database name</info>: ',
-                $notEmptyValidator,
-                10
-            );
-
-            #
-            $yesNoValidator = function($arg) {
-                if (in_array($arg, ['Y', 'y', 'N', 'n'])) {
-                    return $arg;
-                }
-                throw new Exception('This is not a valid answer. Please choose Y, y, N or n.');
-            };
-
-            # Ask if we're happy with this
-            $happyWithDatabaseInput = $io->askAndValidate(
-                '<info>Are you happy with your input for the database configuration? (Default to Y)</info> [<comment>Y,n</comment>]?',
-                $yesNoValidator,
-                10,
-                'Y'
-            );
-
-            # Restart if needed
-            if (in_array($happyWithDatabaseInput, ['N', 'n'])) {
-
-                # Ask if we want to stop setting up a database
-                $cancelDatabaseSequence = $io->askAndValidate(
-                    "<info>Do you want to cancel setting up a database? (N = Re-input database credentials, default to N)</info> [<comment>y,N</comment>]?",
-                    $yesNoValidator,
-                    10,
-                    'N'
-                );
-
-                # Do we not wanna setup a database?
-                if (in_array($cancelDatabaseSequence, ['Y', 'y'])) {
-                    return;
-                }
-
-                $io->write('<info>Please input your database credentials</info>');
-
-                # Start the database sequence again
-                static::startDatabaseSequence($dir, $io);
-
-                return;
-            }
-
-            # Replace the user, password and name in the app.php
-            static::_replaceTagInFile($io, '__DATABASE_USER__', $databaseUser, $appConfigFile);
-            static::_replaceTagInFile($io, '__DATABASE_PASSWORD__', $databasePassword, $appConfigFile);
-            static::_replaceTagInFile($io, '__DATABASE_NAME__', $databaseName, $appConfigFile);
-
-        }
-
-        # Start database sessions sequence
-
-        static::databaseSessions($dir, $io);
-    }
-
-    /**
-     * @param $dir
-     * @param $io IOInterface
-     *
-     * @throws Exception
-     */
-    public static function databaseSessions($dir, $io) {
-
-        $yesNoValidator = function($arg) {
-            if (in_array($arg, ['Y', 'y', 'N', 'n'])) {
-                return $arg;
-            }
-            throw new Exception('This is not a valid answer. Please choose Y, y, N or n.');
-        };
-
-        $io->write('');
-
-        # Ask if we want to use database sessions
-        $doWeWantDatabaseSessions = $io->askAndValidate(
-            "<info>Do you want to setup database sessions? (Default to Y)</info> [<comment>Y,n</comment>]?",
-            $yesNoValidator,
-            10,
-            'Y'
-        );
-
-        # If we dont want database sessions, just return, no hard feelings
-        if (in_array($doWeWantDatabaseSessions, ['N', 'n'])) {
-            return;
-        }
-
-        $appConfigFile = $dir . '/config/app.php';
-
-        $configFile = file_get_contents($appConfigFile);
-
-        # TODO: Make this a better check, there must be a way
-        # TODO: Idea: Cake fetches the app.php as an array I think (seeing as the first line is a return statement)
-        # TODO: So maybe we can too and check it that way
-
-        if (strpos($configFile, "'defaults' => 'php',") === false) {
-
-            $io->write('Seems like you\'re not using php sessions, skipping database session setup');
-
-            return;
-        }
-
-        # TODO: Figure out of this is the proper way to do this
-        # TODO: Because it feels a bit weird to boot up Cake here
-        # TODO: We need it now though, because we're going to execute an sql file using the (newly) set database config in app.php
-
-        # Bootstrap Cake
-        /** @noinspection PhpIncludeInspection */
-        require $dir . '/vendor/autoload.php';
-        /** @noinspection PhpIncludeInspection */
-        require $dir . '/config/bootstrap.php';
-
-        # Get the sessions query
-        $sessionTableQuery = file_get_contents($dir . '/config/schema/sessions.sql');
-
-
-        # TODO: Add check if the table already exists
-        try {
-
-            # Get the db connection
-            $conn = ConnectionManager::get('default');
-
-            # Execute the query
-            $conn->execute($sessionTableQuery);
-        } catch (Exception $e) {
-            $io->write('<error>Something went wrong creationg the sessions table, perhaps the table already exists?</error>');
-
-            return;
-        }
-
-        # Set session config to 'database' instead of php
-        static::_replaceTagInFile($io, "'defaults' => 'php',", "'defaults' => 'database',", $appConfigFile);
-
-        # If all went well, and get here, we have no errors
-        $io->write('<info>Succesfully setup database sessions</info>');
-    }
-
-    /**
-     * @param $io IOInterface
-     * @param $tag
-     * @param $replaceWith
-     * @param $file
-     */
-    private static function _replaceTagInFile($io, $tag, $replaceWith, $file) {
-        $content = file_get_contents($file);
-        $content = str_replace($tag, $replaceWith, $content, $count);
-
-        if ($count == 0) {
-            $io->write('No ' . $tag . ' placeholder to replace.');
-
-            return;
-        }
-
-        $result = file_put_contents($file, $content);
-        if ($result) {
-            $io->write('Updated ' . $tag . ' value in ' . $file);
-
-            return;
-        }
-        $io->write('Unable to update ' . $tag . ' value.');
     }
 }
