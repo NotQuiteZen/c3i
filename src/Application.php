bd2a22aaa8ecbdeaecdf08177d3f5efc51c54873
Milan <milanvanas@gmail.com>
2018-04-29 21:39:10 +0200

    Added bootstrap function so cake bake can load plugins

--- src/Application.php
+++ src/Application.php
@@ -12,6 +12,7 @@
  * @since     3.3.0
  * @license   https://opensource.org/licenses/mit-license.php MIT License
  */
+
 namespace App;
 
 use Cake\Core\Configure;
@@ -26,24 +27,26 @@ use Cake\Routing\Middleware\RoutingMiddleware;
  * This defines the bootstrapping logic and middleware layers you
  * want to use in your application.
  */
-class Application extends BaseApplication
-{
+class Application extends BaseApplication {
+
+    public function bootstrap() {
+        parent::bootstrap();
+    }
+
     /**
      * Setup the middleware queue your application will use.
      *
      * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to setup.
+     *
      * @return \Cake\Http\MiddlewareQueue The updated middleware queue.
      */
-    public function middleware($middlewareQueue)
-    {
+    public function middleware($middlewareQueue) {
         $middlewareQueue
             // Catch any exceptions in the lower layers,
             // and make an error page/response
             ->add(ErrorHandlerMiddleware::class)
-
             // Handle plugin/theme assets like CakePHP normally does.
             ->add(AssetMiddleware::class)
-
             // Add routing middleware.
             // Routes collection cache enabled by default, to disable route caching
             // pass null as cacheConfig, example: `new RoutingMiddleware($this)`
